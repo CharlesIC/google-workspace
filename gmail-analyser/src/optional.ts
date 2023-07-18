9f33d97ff8f320ab115d9d1ae336493ebefe5349
Karol C. Gancarz <karol.sinx@gmail.com>
2023-07-18 21:16:43 +0100

    Add optional util

---
+++ gmail-analyser/src/optional.ts
@@ -0,0 +1,50 @@
+export namespace Utils {
+    export function nonNull<Type>(value: Type | undefined): NonNullable<Type>;
+    export function nonNull<Type, Key extends keyof Type>(object: Type, property: Key): NonNullable<Type[Key]>;
+    export function nonNull<Type, Key extends keyof Type>(object: Type, ...properties: Key[]): NonNullable<Pick<Type, Key>>;
+    export function nonNull<Type, Key extends keyof Type>(object: Type, ...properties: Key[]): NonNullable<Type | Type[Key] | Pick<Type, Key>> {
+        if (properties.length == 0) {
+            return tryGetValue(object);
+        }
+
+        if (properties.length == 1) {
+            return tryGetProperty(object, properties[0]);
+        }
+
+        return tryGetProperties(object, properties);
+    }
+
+    function tryGetValue<Type>(value: Type | undefined): NonNullable<Type> {
+        if (value) {
+            return value;
+        }
+
+        throw new ReferenceError();
+    }
+
+    function tryGetProperty<Type, Key extends keyof Type>(object: Type, property: Key): NonNullable<Type[Key]> {
+        const value = object[property];
+
+        if (value) {
+            return value;
+        }
+
+        throw new ReferenceError(`Property '${property.toString()}' is undefined`);
+    }
+
+    function tryGetProperties<Type, Key extends keyof Type>(object: Type, keys: Key[]): NonNullable<Pick<Type, Key>> {
+        const missing: Key[] = [];
+
+        keys.forEach(key => {
+            if (!object[key]) {
+                missing.push(key);
+            }
+        });
+
+        if (missing.length > 0) {
+            throw new ReferenceError(`Undefined properties (${missing.join(" | ")})`);
+        }
+
+        return object;
+    }
+}
